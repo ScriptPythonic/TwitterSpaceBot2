7edf690bdba23c54f361e010fda4bddf6ccb3ec7
QUADRI BASIT AYOMIDE <115375709+ScriptPythonic@users.noreply.github.com>
2024-07-31 12:46:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from bot import create_app
-import schedule
-import time 
+
 
 app = create_app()
 
 if __name__ == '__main__':
 
     app = create_app()
-    app.run(debug=True, port=4000)
+    app.run(debug=False, port=4000)
 
 
    
